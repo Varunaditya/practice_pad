14bfec29b45f6393e3560a7427feec2f3d5ac530
Varunaditya <varunaditya93@gmail.com>
2018-04-06 20:05:12 -0500

    updated making_anagrams.cpp

--- C++/making_anagrams.cpp
+++ C++/making_anagrams.cpp
@@ -8,8 +8,6 @@ int number_needed(string a, string b) {
     int i,temp; //for iterations
     map<char,int> string_A; //mapping the characters of string_A
     map<char,int> string_B; //mapping the characters of string_B
-    map<char,int> refMapA; //mapping the characters of string_B
-    map<char,int> refMapB; //mapping the characters of string_B
     map<char,int>::iterator it; //variable of class iterator for iterating through the maps
     for(i = 0; i < a.length(); i++){
         string_A[a[i]] = string_A[a[i]] + 1; //counting the number of occurences of letters in string_A
@@ -17,11 +15,9 @@ int number_needed(string a, string b) {
     for(i = 0; i < b.length(); i++){
         string_B[b[i]] = string_B[b[i]] + 1;  //counting the number of occurences of letters in string_B
     }
-    refMapA = string_A;
-    refMapB = string_B;
     for(it = string_A.begin(); it != string_A.end(); it++){
-        if(refMapB[it->first]){
-            temp = (refMapB[it->first] - it->second);
+        if(string_B[it->first]){ //it->first is the key n the mapped data while it->second is the value
+            temp = (string_B[it->first] - it->second);
             if(temp < 0) temp *= (-1);
             requiredNumber += temp;
             }
@@ -30,7 +26,7 @@ int number_needed(string a, string b) {
             }
         }
     for(it = string_B.begin(); it != string_B.end(); it++){
-        if(!refMapA[it->first]){ //looking for characters that are not present in string A.
+        if(!string_A[it->first]){ //looking for characters that are not present in string A.
             requiredNumber += it->second;
             }
         }
