5f8b77f8754df1700b69c81df09ef86f0b54acbd
Varunaditya <varunaditya93@gmail.com>
2018-04-06 22:34:15 -0500

    updated ransome_note.cpp

--- C++/ransom_note.cpp
+++ C++/ransom_note.cpp
@@ -9,6 +9,9 @@ Given the words in the magazine and the words in the ransom note, print
 Yes if he can replicate his ransom note exactly using whole words from 
 the magazine; otherwise, print No.
 
+using map as the hash table. Word would be the key and the value would 
+be the number of times this word appears.
+
 Link: https://www.hackerrank.com/challenges/ctci-ransom-note/problem
 
 */
@@ -18,25 +21,24 @@ Link: https://www.hackerrank.com/challenges/ctci-ransom-note/problem
 #include <vector>
 using namespace std;
 
-bool ransomeNote(vector <string> magazine, vector <string> ransome){
-	if (magazine.size() < ransome.size()) return false;
+bool ransomNote(vector <string> magazine, vector <string> ransom){
+	if (magazine.size() < ransom.size()) return false;
 	int i, flag = 0;
-	map <string, int> magazineMap;
+	map <string, int> magazineMap; 
 	map <string, int> :: iterator it;
 	for(i = 0; i < magazine.size(); i++){
 		magazineMap[magazine[i]] += 1;
 	}
-	for(i = 0; i < ransome.size(); i++){
-		if(magazineMap[ransome[i]]){
-			magazineMap[ransome[i]] = magazineMap[ransome[i]] - 1;
+	for(i = 0; i < ransom.size(); i++){
+		if(magazineMap[ransom[i]]){
+			magazineMap[ransom[i]] = magazineMap[ransom[i]] - 1;
 		}
 		else{
             flag = 1;
             break;
         }
 	}
-	if (flag) return false;
-	else return true;
+	return flag == 1 ? false : true; 
 }
 
 
@@ -44,16 +46,16 @@ int main(){
 	int m; cin >> m;
     int n; cin >> n;
 	string temp;
-    vector <string> magWords, ransomeWords; 
+    vector <string> magWords, ransomWords; 
 	for(int i = 0; i < m; i++){
 		cin >> temp;
         magWords.push_back(temp);
 	}
 	for(int i = 0; i < n; i++){
 		cin >> temp;
-        ransomeWords.push_back(temp);
+        ransomWords.push_back(temp);
     }
-	if (ransomeNote(magWords, ransomeWords)) cout << "Yes\n";
+	if (ransomNote(magWords, ransomWords)) cout << "Yes\n";
 	else cout << "No\n";
 	return 0;
     }
