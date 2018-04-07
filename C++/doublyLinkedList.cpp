4f40cda669a4bc483edc27503adc946439f467cb
Varunaditya <varunaditya93@gmail.com>
2018-04-07 17:03:34 -0500

    initial upload

---
+++ C++/doublyLinkedList.cpp
@@ -0,0 +1,97 @@
+/*
+An implementation of Doubly Linked List.
+*/
+#include <iostream>
+#include <list>
+using namespace std;
+
+struct node{
+	int data;
+	node *next;
+	node *previous;
+};
+
+class linkedList{
+private:
+	node *head, *tail;
+
+public:
+	linkedList(){
+		head = NULL;
+		tail = NULL;
+	}
+
+	void createNode(int value){
+		node *temp = new node();
+		temp->data = value;
+		temp->next = NULL;
+		temp->previous = NULL;
+		head = temp;
+		tail = temp;
+		temp = NULL;
+	}
+
+	void display(){
+		node *temp = new node();
+		temp = head;
+		while(temp != NULL){
+			cout << temp->data << " ";
+			temp = temp->next;
+		}
+		cout << endl;
+	}
+	
+	void push_front(int value){
+		if(head == NULL) createNode(value);
+		else{
+		node *temp = new node();
+		temp->data = value;
+		temp->next = head;
+		temp->previous = NULL;
+		head->previous = temp;
+		head = temp;
+		}
+	}
+
+	void push_back(int value){
+		if(head == NULL) createNode(value);
+		else{
+		node *temp = new node();
+		temp->data = value;
+		temp->next = NULL;
+		temp->previous = tail;
+		tail->next = temp;
+		tail = temp;
+		}
+	}
+
+	void pop_front(){
+		node *temp = new node();
+		temp = head;
+		head = head->next;
+		head->previous = NULL;
+		delete temp;
+	}
+
+	void pop_back(){
+		node *temp = new node();
+		temp = tail;
+		tail = tail->previous;
+		tail->next = NULL;
+		delete temp;
+	}
+};
+
+int main(){
+	linkedList obj;
+	obj.push_back(2);
+	obj.push_front(1);
+	obj.display();
+	obj.push_back(3);
+	obj.display();
+	obj.pop_front();
+	obj.display();
+	obj.pop_front();
+	obj.display();
+	return 0;
+}
