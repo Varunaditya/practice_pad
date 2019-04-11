9b8a1172f3956aa2ee6563296272af62d2835168
Varunaditya Jadwal <varunaditya93@gmail.com>
2019-04-10 21:34:43 -0700

    Update quick_sort.cpp

--- C++/quick_sort.cpp
+++ C++/quick_sort.cpp
@@ -1,3 +1,34 @@
-class Solution{
-  
-}
+class Solution {
+public:
+    int partition(vector<int> &nums, int left, int right, int pivot){
+        while(left <= right){
+            while(nums[left] > pivot){
+                left++;
+            }
+            while(nums[right] < pivot){
+                right--;
+            }
+            if (left <= right){
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+    
+    void quick_sort(vector<int> &nums, int left, int right){
+        if (left >= right) return;
+        int pivot = nums[(left + right) / 2];
+        int position = partition(nums, left, right, pivot);
+        quick_sort(nums, left, position - 1);
+        quick_sort(nums, position, right);
+    }
+    
+    int findKthLargest(vector<int>& nums, int k) {
+        quick_sort(nums, 0, nums.size() -1);
+        return nums[k - 1];
+    }
+};
